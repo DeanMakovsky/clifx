91a17ec97dd83b4572b9b4c90a9facc550851000
DeanMakovsky <deanmkv@gmail.com>
2015-12-23 01:35:24 -0500

    Improved message printing

--- lifx.cpp
+++ lifx.cpp
@@ -12,8 +12,13 @@
 #include <cstdio>
 #include <errno.h>
 
+#include <iomanip> // for cout columns
+#include <bitset>  // for pretty printing odd-sized bit fields
 using namespace std;
 
+// #define tab "\t"
+#define fcol setw(15)
+#define tab setw(10)
 
 /* 
 compile with:
@@ -26,28 +31,29 @@ clang++ -Wall lifx.cpp -std=c++11
 #pragma pack(push, 1)
 class Header {
 protected:
-  /* frame */
-  uint16_t size;
-  uint16_t protocol:12;
-  uint8_t  addressable:1;
-  uint8_t  tagged:1;
-  uint8_t  origin:2;
-  uint32_t source;
-  /* frame address */
-  uint8_t  target[8];
-  uint8_t  reserved[6];
-  uint8_t  res_required:1;
-  uint8_t  ack_required:1;
-  uint8_t  :6;
-  uint8_t  sequence;
-  /* protocol header */
-  uint64_t :64;
-  uint16_t type;
-  uint16_t :16;
-  /* variable length payload follows */
+	/* frame */
+	uint16_t size;
+	uint16_t protocol:12;
+	uint8_t  addressable:1;
+	uint8_t  tagged:1;
+	uint8_t  origin:2;
+	uint32_t source;
+	/* frame address */
+	uint8_t  target[8];
+	uint8_t  reserved[6];
+	uint8_t  res_required:1;
+	uint8_t  ack_required:1;
+	uint8_t  :6;
+	uint8_t  sequence;
+	/* protocol header */
+	uint64_t :64;
+	uint16_t type;
+	uint16_t :16;
+	/* variable length payload follows */
 public:
 	Header();
 	static Header deserialize(int);
+	void printEverything();
 };
 
 class Color: public Header {
@@ -104,17 +110,21 @@ Header::Header() {
 
 /**
 * Reads from a socket the appropriate number of bytes and puts it into a new object.
-* If the message type is unknown, prints error info and returns a regular Header.
+* If the message type is unknown (or payload is empty), returns a regular Header.
+* TODO socket will be non-blocking, so if there is no data, then "type" will be 0.
 */
 Header Header::deserialize(int sockfd) {
+	
+	// set up data structures
 	char buffer[100];
 	memset(buffer, 0, sizeof(buffer));
-	printf("Buffer size: %lu\n", sizeof(buffer));
 
 	struct sockaddr_in sender;
 	socklen_t senderSize = sizeof(sender);
 	socklen_t backup = senderSize;
 
+
+	// get data from socket
 	int ret = recvfrom(sockfd, buffer, sizeof(buffer), 0 ,
 		(sockaddr *) &sender, &senderSize);
 	if (ret == -1) {
@@ -122,25 +132,81 @@ Header Header::deserialize(int sockfd) {
 		printf("Error reading from socket: %d, %s\n", val, strerror(val) );
 	}
 
+
+	// sender diagnostics, mainly for fun
 	if (backup != senderSize) {
 		printf("The sender address of this message changed (%d -> %d)\n", backup, senderSize);
 	}
+	// printf("sin_family: %hd\t", sender.sin_family);
+	// printf("AF_INET := %d\n", AF_INET);
+	char ip_buffer[100];
+	memset(ip_buffer, 0, sizeof(ip_buffer));
+	inet_ntop(AF_INET, &sender.sin_addr.s_addr, ip_buffer, sizeof(ip_buffer));
+	printf("Sending address: %s:%hu\n", ip_buffer, ntohs(sender.sin_port));
 
+
+	// validate the input some
 	printf("Read -%d- bytes.\n", ret);
 	if (ret > sizeof(buffer)) {
-		printf("Read too many bytes, truncating message.\n");
+		printf("Read too many bytes for buffer, truncating message.\n");
 		ret = sizeof(buffer);
 	}
 	Header h;
 	memcpy(&h, buffer, ret);
-	printf("Size: %d\nProtocol: %d\nType: %d\n", h.size, h.protocol, h.type);
-
+	if (h.protocol != 1024) {
+		printf("Incorrect protocol number, probably bad packet.\n");
+	}
+	// printf("Size: %d\nProtocol: %d\nType: %d\n", h.size, h.protocol, h.type);
+	h.printEverything();
 
 
 	Color obj(1,2,3,4,5);
 	printf("Returning size: %lu\n", sizeof(obj));  // TODO this is inconsistent, relearn the virtual keyword
-	return obj;
+	return h;
 }
+
+
+void Header::printEverything() {
+	// /* frame */ 
+	// uint16_t size;
+	// uint16_t protocol:12;
+	// uint8_t  addressable:1;
+	// uint8_t  tagged:1;
+	// uint8_t  origin:2;
+	// uint32_t source;
+	// /* frame address */
+	// uint8_t  target[8];
+	// uint8_t  reserved[6];
+	// uint8_t  res_required:1;
+	// uint8_t  ack_required:1;
+	// uint8_t  :6;
+	// uint8_t  sequence;
+	// /* protocol header */
+	// uint64_t :64;
+	// uint16_t type;
+	// uint16_t :16;
+
+	cout << "~~~~~ Frame ~~~~~" << endl;
+	cout << fcol << "size" << tab << size << endl;
+	cout << fcol << "protocol" << tab << protocol << endl;
+	cout << fcol << "addressable" << tab << bitset<1>(addressable) << endl;
+	cout << fcol << "tagged" << tab << bitset<1>(tagged) << endl;
+	cout << fcol << "origin" << tab << bitset<2>(origin) << endl;
+	cout << fcol << "source" << tab << source << endl;
+	cout << "~~~~~ Frame Address ~~~~~" << endl;
+	cout << fcol << "target" << hex << tab << "0x" << bitset<64>(target).to_ulong() << endl;  // TODO convert this to something usuable
+	cout << fcol << "reserved" << tab << "6 bytes" << endl;
+	cout << fcol << "res_required" << tab << bitset<1>(res_required) << endl;
+	cout << fcol << "ack_required" << tab << bitset<1>(ack_required) << endl;
+	cout << fcol << "reserved" << tab << "6 bits" << endl;
+	cout << fcol << "sequence" << tab << bitset<8>(sequence).to_ulong() << endl;
+	cout << "~~~~~ Protocol Header ~~~~~" << endl;
+	cout << fcol << "reserved" << tab << "8 bytes" << endl;
+	cout << fcol << "type" << tab << type << endl;
+	cout << fcol << "reserved" << tab << "2 bytes" << endl;
+}
+
+
 
 void Color::print() {
 	// TODO look up this stuff for printf
