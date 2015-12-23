b7ba670d2e7c148324f2024d999dbaf736d3d46e
DeanMakovsky <deanmkv@gmail.com>
2015-12-22 22:45:19 -0500

    Working code: changes colors and starting to read network messages

--- lifx.cpp
+++ lifx.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <stdint.h>
 #include <cstring>
+#include <cstdlib>
+#include <thread>
+
+
+#include <arpa/inet.h>  // for inet_pton
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdio>
+#include <errno.h>
+
 using namespace std;
 
+
+/* 
+compile with:
+clang++ -Wall lifx.cpp -std=c++11
+*/
+
+
+
+
 #pragma pack(push, 1)
-typedef struct {
+class Header {
+protected:
   /* frame */
   uint16_t size;
   uint16_t protocol:12;
@@ -24,27 +45,114 @@ typedef struct {
   uint16_t type;
   uint16_t :16;
   /* variable length payload follows */
-} Header;
+public:
+	Header();
+	static Header deserialize(int);
+};
 
-typedef struct {
-	Header h;
+class Color: public Header {
+protected:
 	uint8_t  :8;
 	uint16_t hue:16;
 	uint16_t saturation:16;
 	uint16_t brightness:16;
 	uint16_t kelvin:16;
 	uint32_t duration:32;
-
-} Color;
+public:
+	Color(unsigned int,unsigned int,unsigned int,unsigned int,unsigned int);
+	void print();
+};
 
 #pragma pack(pop)
 
-#include <arpa/inet.h>  // for inet_pton
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <cstdio>
-#include <errno.h>
+
+
+
+Color::Color(unsigned int _hue, unsigned int _sat, unsigned int _bright, unsigned int _kelvin, unsigned int _duration) : Header() {
+	// cout << "Color size (this): " << sizeof(*this) << endl;
+	// cout << "Color size (Header): " << sizeof(Header) << endl;
+	// cout << "Color size (Color): " << sizeof(Color) << endl;
+	
+	size = sizeof(*this);
+
+	// zero out everything except what the superconstructor did
+	memset(((char *)this)+sizeof(Header), 0, sizeof(*this) - sizeof(Header));
+	hue = _hue;
+	saturation = _sat;
+	brightness = _bright;
+	kelvin = _kelvin;
+	duration = _duration;
+}
+
+Header::Header() {
+	// cout << "Header size (this): " << sizeof(*this) << endl;
+	// cout << "Header size (Header): " << sizeof(Header) << endl;
+	// cout << "Header size (Color): " << sizeof(Color) << endl;
+	memset(this, 0, sizeof(*this));
+	size = sizeof(Color);
+	protocol = 1024;
+	tagged = 1;
+	addressable = 1;
+	source = 0;
+
+	// h.target = 0; // for all MACs
+	sequence = 0;
+
+	type = 102;
+}
+
+/**
+* Reads from a socket the appropriate number of bytes and puts it into a new object.
+* If the message type is unknown, prints error info and returns a regular Header.
+*/
+Header Header::deserialize(int sockfd) {
+	char buffer[100];
+	memset(buffer, 0, sizeof(buffer));
+	printf("Buffer size: %lu\n", sizeof(buffer));
+
+	struct sockaddr_in sender;
+	socklen_t senderSize = sizeof(sender);
+	socklen_t backup = senderSize;
+
+	int ret = recvfrom(sockfd, buffer, sizeof(buffer), 0 ,
+		(sockaddr *) &sender, &senderSize);
+	if (ret == -1) {
+		int val = errno;
+		printf("Error reading from socket: %d, %s\n", val, strerror(val) );
+	}
+
+	if (backup != senderSize) {
+		printf("The sender address of this message changed (%d -> %d)\n", backup, senderSize);
+	}
+
+	printf("Read -%d- bytes.\n", ret);
+	if (ret > sizeof(buffer)) {
+		printf("Read too many bytes, truncating message.\n");
+		ret = sizeof(buffer);
+	}
+	Header h;
+	memcpy(&h, buffer, ret);
+	printf("Size: %d\nProtocol: %d\nType: %d\n", h.size, h.protocol, h.type);
+
+
+
+	Color obj(1,2,3,4,5);
+	printf("Returning size: %lu\n", sizeof(obj));  // TODO this is inconsistent, relearn the virtual keyword
+	return obj;
+}
+
+void Color::print() {
+	// TODO look up this stuff for printf
+	printf("structure size : %zu bytes\n",sizeof(*this));
+	for(int i = 0; i < sizeof(*this) ; i++) {
+		// printf("%02x ",this[i]);
+	}
+
+}
+
+
+
 
 struct sockaddr_in makeBCastDest() {
 	struct sockaddr_in dest;
@@ -55,8 +163,16 @@ struct sockaddr_in makeBCastDest() {
 	return dest;
 }
 
-void sendPacket(Color c) {
 
+class MySocket {
+	int fd;
+public:
+	MySocket();
+	void send(void *, int);
+	int getSocket() { return fd; }
+};
+
+MySocket::MySocket() {
 	struct sockaddr_in serverAddress;
 	memset(&serverAddress,0,sizeof(serverAddress));
 
@@ -65,62 +181,64 @@ void sendPacket(Color c) {
 	// inet_pton(AF_INET, "localhost", &serverAddress.sin_addr.s_addr);
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
 	// check for correctness
-	printf("File descriptor: %d\n", fd);
-	if (fd < 0) {
-		printf("%s\n", "Error creating socket!");
+	// printf("File descriptor: %d\n", fd);
+	if (fd == -1) {
+		int val = errno;
+		printf("Error creating socket: %d, %s\n", val, strerror(val) );
 	}
 
 	// allow UDP broadcast
 	int broadcastEnable=1;
-	int ret=setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
-	printf("Socket options return value: %d\n", ret);
-
+	int ret = setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+	// printf("Socket options return value: %d\n", ret);
+	if (ret == -1) {
+		int val = errno;
+		printf("Error setting socket options: %d, %s\n", val, strerror(val) );
+	}
 
 
 	ret = bind(fd, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
-
-	printf("Bind return value: %d\n", ret);
-
+	// printf("Bind return value: %d\n", ret);
+	if (ret == -1) {
+		int val = errno;
+		printf("Error binding socket: %d, %s\n", val, strerror(val) );
+	}
+	
+}
+void MySocket::send(void * start, int len) {
 	struct sockaddr_in destination = makeBCastDest();
 
-	ret = sendto(fd, &c, sizeof(Color), 0, (struct sockaddr *) &destination, sizeof(destination));
+	int ret = sendto(fd, start, len, 0, (struct sockaddr *) &destination, sizeof(destination));
 	printf("Send return val: %d\n", ret);
-	if (ret < 0) {
+	if (ret == -1) {
 		int val = errno;
 		printf("Errno: %d, %s\n", val, strerror(val) );
 	}
 }
+int randy(int min, int max) {
+	int ret = rand();
+	return (ret % (max - min)) + min;
+}
 
 
 int main(int argc, char ** argv) {
-	Header h;
-	memset(&h, 0, sizeof(Header));
-	h.size = sizeof(Color);
-	h.protocol = 1024;
-	h.origin = 3;
-	h.source = 0;
 
-	// h.target = 0; // for all MACs
-	h.sequence = 0;
+	MySocket sock;
 
-	h.type = 102;
+	Header thing = Header::deserialize(sock.getSocket());
+	printf("Returned object size: %lu\n", sizeof(thing));
+
+	return 0;
 
 
 
-	Color c;
-	memset(&c, 0, sizeof(Color));
-	c.h = h;
+	for ( ;; ) {
+		Color c(randy(0,65535),65535,4*4096,randy(2500,9000),10);
+		sock.send(&c, sizeof(c));
+		this_thread::sleep_for (chrono::seconds(1));
+	}
 
-	c.hue = 0;
-	c.saturation = 65535;
-	c.brightness = 4096;
-	c.kelvin = 2500;
-	c.duration = 10;
-
-	sendPacket(c);
-
-	// cout << "size " << sizeof(lx_protocol_header_t) << endl;
 	return 0;
 }
